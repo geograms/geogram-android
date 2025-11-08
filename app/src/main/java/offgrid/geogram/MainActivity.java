07401f2385d6f96304e822b6768bbbf12a2fd7d4
Max Brito <brito@triplecheck.tech>
2025-11-08 09:04:17 +0100

    Add missing View import in MainActivity

--- app/src/main/java/offgrid/geogram/MainActivity.java
+++ app/src/main/java/offgrid/geogram/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
