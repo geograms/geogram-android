c4d5174f83e9d7e82eb62a8c3f02281e42ffc89c
Max Brito <brito@triplecheck.tech>
2025-11-09 08:39:19 +0100

    Bump version to 0.3.0 - Messages improvements release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "offgrid.geogram"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         applicationIdSuffix = "geogram"
